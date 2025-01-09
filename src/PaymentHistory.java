82343d13e07dbb3384127b7036f2ecf3978b0f2b
EVGENY KVEST <evgenykvest@gmail.com>
2025-01-09 20:20:47 +0100

    TODO: toString, history

---
+++ src/PaymentHistory.java
@@ -0,0 +1,16 @@
+public class PaymentHistory  {
+    private String method;
+    private double amount;
+
+    public PaymentHistory(String method, double amount) {
+        this.amount = amount;
+        this.method = method;
+    }
+
+    @Override
+    public String toString(){
+        return
+        "Method |" + method + "|" +
+        "Amount: " + amount + "$";
+    }
+}
